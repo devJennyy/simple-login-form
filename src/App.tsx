de32f4f7f6701da7adc313c40937889768adb5d9
Jenny Pieloor <135243946+devJennyy@users.noreply.github.com>
2024-03-19 23:44:58 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import {
-  FaGoogle,
-  FaLinkedinIn,
-} from "react-icons/fa";
+import { FaGoogle, FaLinkedinIn } from "react-icons/fa";
 import "./App.css";
 import { RiFacebookFill } from "react-icons/ri";
 
@@ -9,6 +6,7 @@ function App() {
   return (
     <>
       <div className="h-full w-full flex md:flex-row justify-between items-center">
+        {/*Background Image*/}
         <div className="md:h-full md:w-1/2 object-cover">
           <img
             src="/img/image.png"
@@ -16,8 +14,10 @@ function App() {
           />
         </div>
 
-        <div className="flex flex-col flex-1 justify-center items-left md:gap-[50px] gap-[30px] w-full md:px-[100px] px-[30px]">
-          <div className="flex flex-row justify-end items-center text-[#323232] gap-5 pb-[50px]">
+        {/*Container*/}
+        <div className="flex md:flex-col flex-col-reverse flex-1 justify-center items-left md:gap-[50px] gap-[30px] w-full md:px-[100px] px-[30px]">
+          {/*Sign-in*/}
+          <div className="flex md:flex-row flex-col justify-end items-center text-[#323232] md:gap-5 gap-[13px] pb-[50px]">
             <p className="text-[14px] md:text-[16px] ">
               Already have an account?
             </p>
@@ -26,61 +26,68 @@ function App() {
             </button>
           </div>
 
-          <div className="flex flex-col justify-start items-start text-[#323232]">
-            <h1 className="md:text-[40px] text-[25px] font-bold">
-              Welcome to
-              <a className="text-[#DF6D73] pl-[9px]">Focus!</a>
-            </h1>
-            <p className="md:text-[16px] text-[14px]">Register your account</p>
-          </div>
-
-          <div className="flex flex-col md:gap-5 gap-[5px] w-full text-left">
-            <div>
-              <h1 className="text-left md:pb-[5px] pb-[3px] md:text-[16px] text-[15px] font-[18px]">
-                Name
+          {/*Body*/}
+          <div className="">
+            <div className="flex flex-col justify-start items-start text-[#323232] md:pb-[50px] pb-[40px] md:pt-0 pt-[50px]">
+              <h1 className="md:text-[40px] text-[25px] font-bold">
+                Welcome to
+                <a className="text-[#DF6D73] pl-[9px]">Focus!</a>
               </h1>
-              <input
-                type="text"
-                className="md:border-2 border-[1px] px-[10px] md:py-[5px] py-[2px] rounded-[5px] w-full active:outline-[#FFB4B4] focus:outline-[#FFB4B4]"
-              />
+              <p className="md:text-[16px] text-[14px]">
+                Register your account
+              </p>
             </div>
-            <div>
-              <h1 className="text-left md:pb-[5px] pb-[3px] md:text-[16px] text-[15px] font-[18px]">
-                Email
-              </h1>
-              <input
-                type="text"
-                className="md:border-2 border-[1px] px-[10px] md:py-[5px] py-[2px] rounded-[5px] w-full active:outline-[#FFB4B4] focus:outline-[#FFB4B4]"
-              />
-            </div>
-            <div>
-              <h1 className="text-left md:pb-[5px] pb-[3px] md:text-[16px] text-[15px] font-[18px]">
-                Password
-              </h1>
-              <input
-                type="text"
-                className="md:border-2 border-[1px] px-[10px] md:py-[5px] py-[2px] rounded-[5px] w-full active:outline-[#FFB4B4] focus:outline-[#FFB4B4]"
-              />
-            </div>
-          </div>
 
-          <div className="w-full text-left">
-            <button className="bg-[#DF6D73] md:text-[16px] text-[14px] color-sky-500 text-white font-semibold md:py-[15px] md:px-[75px] py-[10px] px-[55px] rounded-[100px] hover:bg-[#FFB4B4] hover:scale-90 transition-all">
-              Login
-            </button>
-          </div>
+            <div className="flex flex-col md:gap-5 gap-[5px] w-full text-left md:pb-[50px] pb-[30px]">
+              <div>
+                <h1 className="text-left md:pb-[5px] pb-[3px] md:text-[16px] text-[15px] font-[18px]">
+                  Name
+                </h1>
+                <input
+                  type="text"
+                  className="md:border-2 border-[1px] px-[10px] md:py-[5px] py-[2px] rounded-[5px] w-full active:outline-[#FFB4B4] focus:outline-[#FFB4B4]"
+                />
+              </div>
+              <div>
+                <h1 className="text-left md:pb-[5px] pb-[3px] md:text-[16px] text-[15px] font-[18px]">
+                  Email
+                </h1>
+                <input
+                  type="text"
+                  className="md:border-2 border-[1px] px-[10px] md:py-[5px] py-[2px] rounded-[5px] w-full active:outline-[#FFB4B4] focus:outline-[#FFB4B4]"
+                />
+              </div>
+              <div>
+                <h1 className="text-left md:pb-[5px] pb-[3px] md:text-[16px] text-[15px] font-[18px]">
+                  Password
+                </h1>
+                <input
+                  type="text"
+                  className="md:border-2 border-[1px] px-[10px] md:py-[5px] py-[2px] rounded-[5px] w-full active:outline-[#FFB4B4] focus:outline-[#FFB4B4]"
+                />
+              </div>
+            </div>
 
-          <div className="text-[#323232] w-full text-[14px] md:text-[16px] text-left flex flex-row items-center gap-3">
-            <p>Create account with</p>
-            <button className="md:text-[18px] text-[15px] text-[#DF6D73] md:border-2 border-[1px] border-[#DF6D73] rounded-[100px] p-[5px] hover:scale-90 transition-all ">
-              <RiFacebookFill />
-            </button>
-            <button className="md:text-[17px] text-[15px] text-[#DF6D73] md:border-2 border-[1px] border-[#DF6D73] rounded-[100px] p-[5px] hover:scale-90 transition-all">
-              <FaLinkedinIn />
-            </button>
-            <button className="md:text-[17px] text-[15px] text-[#DF6D73] md:border-2 border-[1px] border-[#DF6D73] rounded-[100px] p-[5px] hover:scale-90 transition-all">
-              <FaGoogle />
-            </button>
+            {/*Login*/}
+            <div className="w-full md:text-left md:pb-[50px] pb-[10px]">
+              <button className="bg-[#DF6D73] md:text-[16px] text-[14px] color-sky-500 text-white font-semibold md:py-[15px] md:px-[75px] py-[10px] px-[120px] rounded-[100px] hover:bg-[#FFB4B4] hover:scale-90 transition-all">
+                Login
+              </button>
+            </div>
+
+            {/*Icons*/}
+            <div className="text-[#323232] w-full text-[14px] md:text-[16px] text-left flex-row items-center gap-3 hidden md:flex">
+              <p>Create account with</p>
+              <button className="md:text-[18px] text-[15px] text-[#DF6D73] md:border-2 border-[1px] border-[#DF6D73] rounded-[100px] p-[5px] hover:scale-90 transition-all ">
+                <RiFacebookFill />
+              </button>
+              <button className="md:text-[17px] text-[15px] text-[#DF6D73] md:border-2 border-[1px] border-[#DF6D73] rounded-[100px] p-[5px] hover:scale-90 transition-all">
+                <FaLinkedinIn />
+              </button>
+              <button className="md:text-[17px] text-[15px] text-[#DF6D73] md:border-2 border-[1px] border-[#DF6D73] rounded-[100px] p-[5px] hover:scale-90 transition-all">
+                <FaGoogle />
+              </button>
+            </div>
           </div>
         </div>
       </div>
